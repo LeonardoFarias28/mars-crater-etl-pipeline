02077ff390f133fb3c920db4a108c619d8e63d0c
Leonardo Farias <leonardosfariaas@gmail.com>
2026-03-25 19:41:04 -0300

    Added new columns to improve data analysis

--- src/validate/validate.py
+++ src/validate/validate.py
@@ -5,6 +5,6 @@ def validate_data(df):
     if df["DIAM_CIRC_IMG"].isnull().any():
         raise Exception("Null values found in diameter.")
 
-    df = df[["CRATER_ID", "LAT_CIRC_IMG", "LON_CIRC_IMG", "DIAM_CIRC_IMG"]]
+    df = df[["CRATER_ID", "LAT_CIRC_IMG", "LON_CIRC_IMG", "DIAM_CIRC_IMG","DIAM_ELLI_MAJOR_IMG","DIAM_ELLI_MINOR_IMG","DIAM_ELLI_ECCEN_IMG","DIAM_ELLI_ANGLE_IMG"]]
 
     return df
