0d750a6703043fa7cc9abf27d7eba7e38b333f7e
Leonardo Farias <leonardosfariaas@gmail.com>
2026-03-25 20:56:34 -0300

    add save file csv

--- src/validate/validate.py
+++ src/validate/validate.py
@@ -1,3 +1,4 @@
+import os
 def validate_data(df):
     if df.empty:
         raise Exception("DataFrame empty")
@@ -6,5 +7,12 @@ def validate_data(df):
         raise Exception("Null values found in diameter.")
 
     df = df[["CRATER_ID", "LAT_CIRC_IMG", "LON_CIRC_IMG", "DIAM_CIRC_IMG","DIAM_ELLI_MAJOR_IMG","DIAM_ELLI_MINOR_IMG","DIAM_ELLI_ECCEN_IMG","DIAM_ELLI_ANGLE_IMG"]]
+    output_path = r"C:\Users\Leonardo\Downloads\mars_data\mars_processed.csv"
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    df.to_csv(
+        output_path,
+        index=False
+    )
 
     return df
